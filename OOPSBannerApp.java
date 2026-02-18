c075365ca1a8a0eb550cdd78573439821742f1e9
Preetham0000 <prh5336@gmail.com>
2026-02-18 16:32:19 +0530

    Coded to Render OOPS as Banner using InlineArray

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,39 +1,38 @@
 /**
- * OOPSBannerApp UC4 - Render OOPS as Banner using String Array and Loop
+ * OOPSBannerApp UC5 - Render OOPS as Banner using Inline Array Initialization 
  *
- * This use case improves upon UC3 by using a String array to store banner lines 
- * ad iterating through them with a for-each loop, eliminating hardcoded print 
- * statements and imrpoving modulatrity and reusability.
+ * This use case extends UC4 by defining and populating the String array in a more 
+ * concise way at the time of declaration using String.join() method to create each
+ * line of the banner. This further enhances code readability and maintainability.
  *
  * @author Developer
- * @version 4
+ * @version 5.0
  */
 
-// Extend the user story 3 to further develop the OOPS Banner Application 
-// by using string arrays to hold banner lines and priniting them in a loop.
-// this use case improves upon UC3 by using a String array to store banner lines 
-// and iterating through them with a for-each loop, eliminating hardcoded print
-// statements and improving modularity and reusability.
+// Extend the user story 4 to further developer the OOPS Banner Application by 
+// not only using String arrays to hold banner lines and printing lines and printing them in a 
+// at the time of declation using String.join() method to create each line 
+// of the banner.
 
-public class OOPSBannerApp {
-
-	public static void main(String[] args) {
-
-		String lines[] = new String[7];
-
-        	lines[0] = String.join(" ","  *****   ", "  *****   ", "*******  ", "  *****   ");
-        	lines[1] = String.join(" "," *     *  ", " *     *  ", "*     *  ", " *     *  ");
-        	lines[2] = String.join(" ","*       * ", "*       * ", "*     *  ", "*         ");
-        	lines[3] = String.join(" ","*       * ", "*       * ", "*******  ", " *****    ");
-        	lines[4] = String.join(" ","*       * ", "*       * ", "*        ", "      *   ");
-        	lines[5] = String.join(" "," *     *  ", " *     *  ", "*        ", " *     *  ");
-        	lines[6] = String.join(" ","  *****   ", "  *****   ", "*        ", "  *****   ");
+public class OOPSBannerApp{
 	
-		for (String line : lines) {
+	public static void main(String[] args) {
+		String[] lines = {
+			String.join(" ", "  *****   ", "  *****   ", "*******  ", "  *****   "),
+			String.join(" ", " *     *  ", " *     *  ", "*     *  ", " *     *  "),
+			String.join(" ", "*       * ", "*       * ", "*     *  ", "*         "),
+			String.join(" ", "*       * ", "*       * ", "*******  ", " *****    "),
+			String.join(" ", "*       * ", "*       * ", "*        ", "      *   "),
+			String.join(" ", " *     *  ", " *     *  ", "*        ", " *     *  "),
+			String.join(" ", "  *****   ", "  *****   ", "*        ", "  *****   ")
+		};
+		
+
+		for (String line:lines) {
 			System.out.println(line);
 		}
-
-    }
+		
+	}
 }
 
 
